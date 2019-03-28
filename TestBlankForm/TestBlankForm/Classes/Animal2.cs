9f8df559a1d51ecbd04f9e0cec63c2eba99e2620
christophers-github <chris@projectpolyculture.com>
2019-03-28 11:21:21 -0500

    Added Web References to iOS, Android * TestBlankForm.iOS.csproj: * TestBlankForm.Android.csproj:
    * Animal2.cs: Commented out the xml doc retrieval because it was
      giving an error on Build

--- TestBlankForm/TestBlankForm/Classes/Animal2.cs
+++ TestBlankForm/TestBlankForm/Classes/Animal2.cs
@@ -9,9 +9,8 @@ using System.Xml.Serialization;
 namespace TestBlankForm.Classes
 {
     //maybe wrong place for this??
-    XmlDocument xmlDoc = new XmlDocument();
-    xmlDoc.Load("http://10.101.4.18/ReportServer?%2FPetPoint%20Reports%2FAnimalInventory&amp;rs%3ACommand=Render&amp;rs%3AFormat=XML&amp;rs%3ASessionID=rl0gfl55fvbti545bkvzixf1&amp;rc%3ASchema=True
-");
+    //XmlDocument xmlDoc = new XmlDocument();
+    //xmlDoc.Load("http://10.101.4.18/ReportServer?%2FPetPoint%20Reports%2FAnimalInventory&amp;rs%3ACommand=Render&amp;rs%3AFormat=XML&amp;rs%3ASessionID=rl0gfl55fvbti545bkvzixf1&amp;rc%3ASchema=True");
 
 
     public class Animal2
